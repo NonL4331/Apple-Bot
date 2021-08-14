ebc91f50c28277bee27d6f9e303000a133825fc2
NonL4331 <nicholasl0003@gmail.com>
2021-08-14 18:49:09 +1000

    Init commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use crate::pokemon::pokedex::GET_RANDOM_POKEMON_COMMAND;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+
+
+
+
+
+use std::env;
+
+mod pokemon;
+
+#[group]
+#[commands(ping)]
+#[commands(get_random_pokemon)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(e) = client.start().await {
+        println!("An error occurred while running the client: {:?}", e);
+    }
+
+}
+
+
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
 }
